422789361498e648017a2cb68276f08091f7057f
pisomanik <ajibaa@gmail.com>
2022-07-29 22:51:07 +0530

    feat: past 24H token, pool volumes

--- index.ts
+++ index.ts
@@ -41,6 +41,42 @@ app.get('/table', async (req, res) => {
   res.send(result);
 });
 
+app.get('/table/past24H', async (req, res) => {
+  let now = new Date();
+  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+
+  const data = await pool.query(
+    'select * from arbitoor_txns where blocktime between $1 and $2;',
+    [past24H.getTime(), now.getTime()]
+  );
+
+  res.send(data.rows);
+});
+
+app.get('/pools/past24H', async (req, res) => {
+  let now = new Date();
+  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+
+  const data = await pool.query(
+    'select sum(amount_in) as total_amount_in, sum(amount_out) as total_amount_out, pool_id, dex from arbitoor_txns where blocktime between $1 and $2 group by dex, pool_id;',
+    [past24H.getTime(), now.getTime()]
+  );
+
+  res.send(data.rows);
+});
+
+app.get('/tokens/past24H', async (req, res) => {
+  let now = new Date();
+  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+
+  const data = await pool.query(
+    'select sum(amount_in) as total_amount_in, sum(amount_out) as total_amount_out, token_in, token_out from arbitoor_txns where blocktime between $1 and $2 group by token_in, token_out;',
+    [past24H.getTime(), now.getTime()]
+  );
+
+  res.send(data.rows);
+});
+
 app.listen(5000, () => console.log('Running'));
 
 // TYPES
