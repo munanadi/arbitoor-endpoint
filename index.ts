3f4cbdb00cd9446b0f1b77e7ac3128e38051b8ed
pisomanik <ajibaa@gmail.com>
2022-08-16 03:58:18 +0530

    feat: new DB, same endpoints

--- index.ts
+++ index.ts
@@ -33,27 +33,7 @@ app.get('/', (req, res) => res.send('Hello'));
 app.get('/table', async (req, res) => {
   const data = await pool.query('SELECT * FROM arbitoor_txns');
 
-  let result = `<table>`;
-
-  for (const row of data.rows) {
-    result.concat(`<tr>
-      <td> ${row.receipt_id} </td>
-      <td> ${row.block_height}  </td>
-      <td> ${row.blocktime} </td>
-      <td> ${row.dex} </td>
-      <td> ${row.sender} </td>
-      <td> ${row.success} </td>
-      <td> ${row.amount_in} </td>
-      <td> ${row.amount_out} </td>
-      <td> ${row.pool_id} </td>
-      <td> ${row.token_in} </td>
-      <td> ${row.token_out} </td>
-    </tr>`);
-  }
-
-  result += `<table>`;
-
-  res.send(result);
+  res.send(JSON.stringify(data.rows));
 });
 
 app.get('/table/past24H', async (req, res) => {
@@ -83,34 +63,37 @@ app.get('/pools/past24H', async (req, res) => {
 // Fetches 24H token volume
 app.get('/tokens-volume/past-24H', async (req, res) => {
   let now = new Date();
-  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24 * 1);
 
   const tokensInData = await pool.query(
-    'select sum(amount_in) as total_amount, token_in from arbitoor_txns where blocktime between $1 and $2 group by token_in;',
+    'select sum(amount_in_d) as total_amount, token_in from arbitoor_txns where blocktime between $1 and $2 group by token_in;',
     [past24H.getTime(), now.getTime()]
   );
 
   const tokensOutData = await pool.query(
-    'select sum(amount_out) as total_amount, token_out from arbitoor_txns where blocktime between $1 and $2 group by token_out;',
+    'select sum(amount_out_d) as total_amount, token_out from arbitoor_txns where blocktime between $1 and $2 group by token_out;',
     [past24H.getTime(), now.getTime()]
   );
 
   const tokensResult = tokensInData.rows.concat(tokensOutData.rows);
 
-  const tokensOut = new Map<string, string>();
+  const tokensOut: {
+    [tokenAdd: string]: string;
+  } = {};
 
   for (const row of tokensResult) {
     const tokenAdd = row.token_in ?? row.token_out;
-    let tokenAmount = new Big(row.total_amount.toString());
-    if (tokensOut.get(tokenAdd)) {
-      tokenAmount.add(new Big(tokensOut.get(tokenAdd) ?? '0'));
+
+    if (!tokensOut[tokenAdd]) {
+      tokensOut[tokenAdd] = row.total_amount;
+      continue;
     }
-    tokensOut.set(tokenAdd, tokenAmount.toString());
+
+    let tokenAmountSum = (tokensOut[tokenAdd] ?? 0) + +row.total_amount;
+    tokensOut[tokenAdd] = tokenAmountSum;
   }
 
-  // console.log(tokensOut);
-
-  res.send(Array.from(tokensOut));
+  res.send(JSON.stringify(tokensOut));
 });
 
 // Fetches token volume from beggining
@@ -144,50 +127,34 @@ app.get('/tokens-volume', async (req, res) => {
 // Fetches dexes volume for past 24H
 app.get('/dexes-volume/past-24H', async (req, res) => {
   let now = new Date();
-  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+  let past24H = new Date(now.getTime() - 1000 * 60 * 60 * 24 * 3);
 
   const result = await pool.query(
-    'select sum(amount_out) as amount_out, sum(amount_in) as amount_in, token_in, token_out, dex from arbitoor_txns where blocktime between $1 and $2  group by dex, token_in, token_out;',
+    'select sum(amount_out$_d) as amount_out, sum(amount_in$_d) as amount_in, token_in, token_out, dex from arbitoor_txns where blocktime between $1 and $2  group by dex, token_in, token_out;',
     [past24H.getTime(), now.getTime()]
   );
 
-  const dexesOut = new Map<string, string>();
+  const dexesOut: {
+    [dex: string]: string;
+  } = {};
 
   for (const row of result.rows) {
-    const tokenIn = row.token_in;
-    const tokenOut = row.token_out;
     const amountIn = row.amount_in;
     const amountOut = row.amount_out;
     const dex = row.dex;
 
-    let tokenInDecimals =
-      tokensMap.get(tokenIn)?.decimals ??
-      (await provider.getTokenMetadata(tokenIn))?.decimals ??
-      1;
-    let tokenOutDecimals =
-      tokensMap.get(tokenOut)?.decimals ??
-      (await provider.getTokenMetadata(tokenOut))?.decimals ??
-      1;
+    let totalTokensAmount = +amountIn + +amountOut;
 
-    let totalTokensAmountIn = new Big(amountIn).mul(
-      new Big('10').pow(-tokenInDecimals)
-    );
-    let totalTokensAmountOut = new Big(amountOut).mul(
-      new Big('10').pow(-tokenOutDecimals)
-    );
-
-    let totalTokensAmount = totalTokensAmountIn.add(totalTokensAmountOut);
-
-    if (dexesOut.get(dex)) {
-      totalTokensAmount.add(new Big(dexesOut.get(dex) ?? '0'));
+    if (!dexesOut[dex]) {
+      dexesOut[dex] = totalTokensAmount.toString();
+    } else {
+      dexesOut[dex] = (+totalTokensAmount + +dexesOut[dex]).toString();
     }
-
-    dexesOut.set(dex, totalTokensAmount.toString());
   }
 
   // console.log(dexesOut);
 
-  res.send(Array.from(dexesOut));
+  res.send(JSON.stringify(dexesOut));
 });
 
 // Fetches dexes volume from beg
